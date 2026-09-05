17aef5eb99c8280b4609096241ce0aab26d080b4
taxizoran1967-byte <323078362+taxizoran1967-byte@users.noreply.github.com>
2026-09-05 08:38:29 +0200

    Update freetype recipe to use SourceForge mirror
    Refactor freetype recipe to subclass and override URL

--- p4a-recipes/freetype/__init__.py
+++ p4a-recipes/freetype/__init__.py
@@ -1,4 +1,3 @@
-
 # Lokalni p4a recept koji GAZI ugradjeni "freetype" recept.
 #
 # ZASTO POSTOJI OVAJ FAJL:
@@ -10,10 +9,13 @@
 # STA OVAJ FAJL RADI:
 # Ne pise se ceo recept iznova (rizicno - izgubili bismo patch-eve i
 # configure flagove koje ugradjeni recept koristi). Umesto toga,
-# uvozimo VEC POSTOJECI recept objekat iz p4a i menjamo mu samo
-# atribut "url" da pokazuje na SourceForge mirror, koji freetype
-# projekat i dalje zvanicno odrzava. Sve ostalo (verzija, patch-evi,
-# build_arch metoda, zavisnosti) ostaje potpuno isto kao u originalu.
+# PRAVIMO PODKLASU vec postojece recept klase i u njoj definisemo
+# svoj "url" kao obicno polje. To automatski "pregazi" atribut iz
+# roditeljske klase - bez obzira da li je tamo obicna promenljiva
+# ili (kao u ovoj verziji p4a-a) @property koja se racuna u kodu i
+# ne moze se direktno menjati spolja. Sve ostalo (verzija, patch-evi,
+# build_arch metoda, zavisnosti) ostaje potpuno isto kao u originalu,
+# jer nasledjujemo pravu klasu, ne prepisujemo je.
 #
 # Da bi ovaj fajl bio pronadjen, u buildozer.spec mora postojati:
 #   p4a.local_recipes = ./p4a-recipes
@@ -21,13 +23,22 @@
 
 from pythonforandroid.recipes.freetype import recipe as _izvorni_recept
 
-# Originalni URL (za referencu, ne koristi se):
-#   https://download.savannah.gnu.org/releases/freetype/freetype-{version}.tar.gz
-#
-# Novi URL - zvanicni SourceForge mirror, isti fajl, ista verzija:
-_izvorni_recept.url = (
-    "https://downloads.sourceforge.net/project/freetype/freetype2/"
-    "{version}/freetype-{version}.tar.gz"
-)
+# Uzimamo STVARNU klasu originalnog recepta (ne moramo da znamo njeno
+# ime napamet - ovo radi bez obzira kako se klasa zapravo zove).
+_OriginalnaKlasa = type(_izvorni_recept)
 
-recipe = _izvorni_recept
+
+class _FreetypeMirrorRecipe(_OriginalnaKlasa):
+    # Originalni URL (za referencu, ne koristi se):
+    #   https://download.savannah.gnu.org/releases/freetype/freetype-{version}.tar.gz
+    #
+    # Novi URL - zvanicni SourceForge mirror, isti fajl, ista verzija.
+    # Definisan kao obicno polje klase - ovo pregazi @property iz
+    # roditeljske klase.
+    url = (
+        "https://downloads.sourceforge.net/project/freetype/freetype2/"
+        "{version}/freetype-{version}.tar.gz"
+    )
+
+
+recipe = _FreetypeMirrorRecipe()
