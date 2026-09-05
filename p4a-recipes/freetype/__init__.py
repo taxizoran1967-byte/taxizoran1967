141337e913901f0f23b83d500cfd7b791a6267c7
taxizoran1967-byte <323078362+taxizoran1967-byte@users.noreply.github.com>
2026-09-05 08:11:44 +0200

    Override freetype recipe to use SourceForge mirror
    This local recipe overrides the built-in freetype recipe to use a more reliable SourceForge mirror for downloading the freetype source code, while preserving all original attributes.

--- p4a-recipes/freetype/__init__.py
+++ p4a-recipes/freetype/__init__.py
@@ -1,1 +1,33 @@
 
+# Lokalni p4a recept koji GAZI ugradjeni "freetype" recept.
+#
+# ZASTO POSTOJI OVAJ FAJL:
+# Ugradjeni p4a recept skida freetype izvorni kod sa
+# https://download.savannah.gnu.org/... - taj server je hronicno
+# nepouzdan (cesto vraca 502/504 greske), sto je uzrok povremenog
+# padanja GitHub Actions build-a na koraku "Downloading freetype".
+#
+# STA OVAJ FAJL RADI:
+# Ne pise se ceo recept iznova (rizicno - izgubili bismo patch-eve i
+# configure flagove koje ugradjeni recept koristi). Umesto toga,
+# uvozimo VEC POSTOJECI recept objekat iz p4a i menjamo mu samo
+# atribut "url" da pokazuje na SourceForge mirror, koji freetype
+# projekat i dalje zvanicno odrzava. Sve ostalo (verzija, patch-evi,
+# build_arch metoda, zavisnosti) ostaje potpuno isto kao u originalu.
+#
+# Da bi ovaj fajl bio pronadjen, u buildozer.spec mora postojati:
+#   p4a.local_recipes = ./p4a-recipes
+# (vidi izmenu u buildozer.spec)
+
+from pythonforandroid.recipes.freetype import recipe as _izvorni_recept
+
+# Originalni URL (za referencu, ne koristi se):
+#   https://download.savannah.gnu.org/releases/freetype/freetype-{version}.tar.gz
+#
+# Novi URL - zvanicni SourceForge mirror, isti fajl, ista verzija:
+_izvorni_recept.url = (
+    "https://downloads.sourceforge.net/project/freetype/freetype2/"
+    "{version}/freetype-{version}.tar.gz"
+)
+
+recipe = _izvorni_recept
